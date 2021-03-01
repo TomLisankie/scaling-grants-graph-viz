d4d04e506ef23e6d4f7e2523d42acc88bd89b789
Thomas Lisankie <id@tomlisankie.com>
2021-03-01 16:35:56 -0600

    Now loads nodes from CSV onto graph. Only selects a random sample though since it can't seem to handle big graphs

--- src/index.js
+++ src/index.js
@@ -3,30 +3,74 @@ import * as Papa from "papaparse";
 window.onload = (event) => {
     var filePicker = document.getElementById("csvPicker");
     filePicker.addEventListener("change", loadCSVs, false);
-    setUpGraph();
 };
 
 var allRows = [];
 
 function loadCSVs() {
-    let csvFiles = this.files;
-    for (const csvFile of csvFiles) {
-        Papa.parse(csvFile, {
-            header: true,
-            complete : function(results) {
-                allRows.push(results.data);
-                console.log("Another CSV file added");
-                allRows = allRows.flat();
-                console.log(allRows.length);
-            }
+    let csvFiles = Array.from(this.files);
+    console.log(csvFiles);
+    Promise.all(csvFiles
+                .map(
+                    csvFile =>
+                    new Promise(
+                        (resolve, reject) =>
+                        Papa.parse(csvFile, {
+                            header: true,
+                            complete : resolve,
+                            error : reject}))
+                        .then(
+                            function(results) {
+                                allRows.push(results.data);
+                                console.log("Another CSV file added");
+                                allRows = allRows.flat();
+                                console.log(allRows.length);
+                            })))
+        .then(function(results) {
+            console.log(allRows[3000]);
+            setUpGraph(allRows);
+        })
+        .catch(function(err) {
+            console.log(err);
         });
-    }
 }
 
-function setUpGraph () {
+function setUpGraph (nodes) {
+
+    var dots = [
+        [
+            'digraph  {',
+            '    node [style="filled"]',
+        ],
+    ];
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    function addDot(dot) {
+        let mediaWikiTitle = dot["MediaWiki Title"]
+            ?
+            dot["MediaWiki Title"].trim()
+            .replace(/[^a-zA-Z0-9]/g, '')
+            :
+             "undefined";
+        dots[0].push('    ' + mediaWikiTitle + ' [fillcolor="#2ca02c", shape="circle"]');
+    }
+
+    let allRowsLength = allRows.length;
+    for(var i = 0; i < 100; i++) {
+        let rowIndex = getRandomInt(allRowsLength);
+        addDot(allRows[rowIndex]);
+    }
+
+    // allRows.forEach(row => addDot(row));
+
+    dots[0].push('}');
+
     var dotIndex = 0;
     var graphviz = d3.select("#graph").graphviz()
-        .logEvents(true)
+        .logEvents(false)
         .on("initEnd", render);
 
     function render() {
@@ -40,105 +84,6 @@ function setUpGraph () {
             });
     }
 
-    var dots = [
-        /* [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    a [fillcolor="#d62728"]',
-         *     '    b [fillcolor="#1f77b4"]',
-         *     '    a -> b',
-         *     '}'
-         * ], */
-        [
-            'digraph  {',
-            '    node [style="filled"]',
-            '    a [fillcolor="#d62728", shape="circle"]',
-            '    c [fillcolor="#2ca02c"]',
-            '    b [fillcolor="#1f77b4"]',
-            '    a -> b',
-            '    a -> c',
-            '}'
-        ],
-        /* [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    a [fillcolor="#d62728"]',
-         *     '    b [fillcolor="#1f77b4"]',
-         *     '    c [fillcolor="#2ca02c"]',
-         *     '    a -> b',
-         *     '    a -> c',
-         *     '}'
-         * ],
-         * [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    a [fillcolor="#d62728", shape="box"]',
-         *     '    b [fillcolor="#1f77b4", shape="parallelogram"]',
-         *     '    c [fillcolor="#2ca02c", shape="pentagon"]',
-         *     '    a -> b',
-         *     '    a -> c',
-         *     '    b -> c',
-         *     '}'
-         * ],
-         * [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    a [fillcolor="yellow", shape="star"]',
-         *     '    b [fillcolor="yellow", shape="star"]',
-         *     '    c [fillcolor="yellow", shape="star"]',
-         *     '    a -> b',
-         *     '    a -> c',
-         *     '    b -> c',
-         *     '}'
-         * ],
-         * [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    a [fillcolor="#d62728", shape="triangle"]',
-         *     '    b [fillcolor="#1f77b4", shape="diamond"]',
-         *     '    c [fillcolor="#2ca02c", shape="trapezium"]',
-         *     '    a -> b',
-         *     '    a -> c',
-         *     '    b -> c',
-         *     '}'
-         * ],
-         * [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    a [fillcolor="#d62728", shape="box"]',
-         *     '    b [fillcolor="#1f77b4", shape="parallelogram"]',
-         *     '    c [fillcolor="#2ca02c", shape="pentagon"]',
-         *     '    a -> b',
-         *     '    a -> c',
-         *     '    b -> c',
-         *     '}'
-         * ],
-         * [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    a [fillcolor="#d62728"]',
-         *     '    b [fillcolor="#1f77b4"]',
-         *     '    c [fillcolor="#2ca02c"]',
-         *     '    a -> b',
-         *     '    a -> c',
-         *     '    c -> b',
-         *     '}'
-         * ],
-         * [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    b [fillcolor="#1f77b4"]',
-         *     '    c [fillcolor="#2ca02c"]',
-         *     '    c -> b',
-         *     '}'
-         * ],
-         * [
-         *     'digraph  {',
-         *     '    node [style="filled"]',
-         *     '    b [fillcolor="#1f77b4"]',
-         *     '}'
-         * ], */
-    ];
 }
 
 export default allRows;
