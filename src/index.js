7cff5c2b692796bdb9526b77f128cc005db81ace
Thomas Lisankie <id@tomlisankie.com>
2021-03-09 17:27:48 -0600

    Can now group-by city

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ window.onload = (event) => {
 };
 
 var allRows = [];
-var done = false;
-var done2 = false;
+var cy;
 
 function loadCSVs() {
     let csvFiles = Array.from(this.files);
@@ -43,18 +42,30 @@ function setUpGraph (nodes) {
 
     var graphElements = competitions.map(function(competitionName) {
         return {
-            data : {id: competitionName}
+            data : {
+                id: competitionName,
+                competition: true
+            },
+            style : {
+                label: "data(id)",
+                'background-color' : 'orange'
+            }
         };});
 
     var i = 0;
     for (let node of nodes) {
-        graphElements.push({
-            data : {
-                id : node["MediaWiki Title"],
-                city : node["City"],
-                country : node["Org Country"]
+        if (node["City"] != undefined) {
+            if (node["MediaWiki Title"].length == 2) {
+                console.log(node["City"]);
             }
-        });
+            graphElements.push({
+                data : {
+                    id : node["MediaWiki Title"],
+                    city : node["City"],
+                    country : node["Org Country"]
+                }
+            });
+        }
         graphElements.push({
             data : {
                 id: i,
@@ -67,18 +78,17 @@ function setUpGraph (nodes) {
         i += 1;
     }
 
-    var cy = cytoscape({
+    cy = cytoscape({
         container : document.getElementById("graph"),
         elements: graphElements,
         layout: {
-            name: 'breadthfirst',
+            name: 'concentric',
             rows: 1
         },
         style: [
             {
                 selector: 'node',
                 style: {
-                    'label': 'data(id)',
                     'background-color': 'blue'
                 }
             },
@@ -91,7 +101,6 @@ function setUpGraph (nodes) {
         ]
     });
 
-    console.log(cy.elements());
     cy.nodes().on('click', function(e){
         var ele = e.target;
         document.getElementById("info").innerHTML =
@@ -100,9 +109,62 @@ function setUpGraph (nodes) {
             "<tr>" + "<td>" + "City: " + ele._private.data.city + "</td>" + "</tr>" +
             "<tr>" + "<td>" + "Country: " + ele._private.data.country + "</td>" + "</tr>" +
             "</table>";
-        console.log('clicked ' + ele.id());
     });
+
+    document.getElementById("groups").addEventListener("change", groupBy);
 
 }
 
-export default allRows;
+function getNodesToGroupAround(fieldName, type) {
+    const nodes = Array.from(new Set(allRows.map(row => row[fieldName])));
+
+    var graphElements = nodes.map(function(instanceName) {
+        var ele = {
+            data : {
+                id: instanceName,
+                competition: true
+            },
+            style : {
+                // label: "data(id)",
+                'background-color' : 'orange'
+            }
+        };
+        ele.data[type] = true;
+        return ele;
+    });
+    return graphElements;
+}
+
+function getEdges(fieldName) {
+    var i = 0;
+    var graphElements = [];
+    for(let node of allRows) {
+        graphElements.push({
+            data : {
+                id: i,
+                source: node["MediaWiki Title"],
+                target: node[fieldName]}
+        });
+        i += 1;
+    }
+    return graphElements;
+}
+
+function groupBy() {
+    let group = document.getElementById("groups").value;
+    if(group == "city") {
+        console.log(group);
+        cy.remove("node[competition]");
+        let cityNodeEles = getNodesToGroupAround("City", group);
+        let cityEdgeEles = getEdges("City");
+        cy.add(cityNodeEles);
+        cy.add(cityEdgeEles);
+        var layout = cy.layout({
+            name: "cose"
+        });
+        cy.animate();
+        layout.run();
+    } else if (group == "competition") {
+        cy.remove("node[city]");
+    }
+}
